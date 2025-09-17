bbb00801ad2827436c55cc48d59285b825eb823b
Bruno Azevedo <onurbazv@gmail.com>
2025-09-17 17:32:52 -0300

    started integration tests

---
+++ apps/backend/test/setup.ts
@@ -0,0 +1,15 @@
+import { execSync } from 'child_process';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+module.exports = () => {
+  process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
+  execSync('npx prisma db push --force-reset --accept-data-loss', {
+    stdio: 'inherit',
+    env: {
+      ...process.env, // passa todas as envs
+      DATABASE_URL: process.env.DATABASE_URL_TEST,
+    },
+  });
+};
