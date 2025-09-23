61aedc72e3caccb5e318b5f1e401873289c74b99
Bruno Azevedo <onurbazv@gmail.com>
2025-09-23 11:21:44 -0300

    restarted frontend with shadcn.ui

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,30 +1,10 @@
-import { useState } from 'react'
-// import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="min-h-screen bg-yellow-200 p-8">
-      <div className="mx-auto w-fit flex gap-2">
-        <div>
-          <button
-            onClick={() => setCount((count) => count - 1)}
-            className="rounded bg-yellow-400 px-4 py-2 text-black hover:bg-yellow-300"
-          >
-            -
-          </button>
-        </div>
-        <h1 className="text-3xl font-bold">Count {count}</h1>
-        <div>
-          <button
-            onClick={() => setCount((count) => count + 1)}
-            className="rounded bg-yellow-400 px-4 py-2 text-black hover:bg-yellow-300"
-          >
-            +
-          </button>
-        </div>
-      </div>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <p className="text-primary">Testando a cor primaria</p>
     </div>
   )
 }
